8e8493ca5cccd00c20c9fab1062f6bd4b4d4e071
Nathan Hartman <nathanial.hartman@gmail.com>
2014-07-10 01:51:50 -0700

    renamed tutorial to salamancer

--- SalamancerApplication.cpp
+++ SalamancerApplication.cpp
@@ -1,70 +1,32 @@
-/*
------------------------------------------------------------------------------
-Filename:    TutorialApplication.cpp
------------------------------------------------------------------------------
-
-This source file is part of the
-   ___                 __    __ _ _    _ 
-  /___\__ _ _ __ ___  / / /\ \ (_) | _(_)
- //  // _` | '__/ _ \ \ \/  \/ / | |/ / |
-/ \_// (_| | | |  __/  \  /\  /| |   <| |
-\___/ \__, |_|  \___|   \/  \/ |_|_|\_\_|
-      |___/                              
-      Tutorial Framework
-      http://www.ogre3d.org/tikiwiki/
------------------------------------------------------------------------------
-*/
 #include "SalamancerApplication.h"
 
 using namespace Ogre;
 //-------------------------------------------------------------------------------------
-TutorialApplication::TutorialApplication(void)
+SalamancerApplication::SalamancerApplication(void)
 {
 }
 //-------------------------------------------------------------------------------------
-TutorialApplication::~TutorialApplication(void)
+SalamancerApplication::~SalamancerApplication(void)
 {
 }
 
 //-------------------------------------------------------------------------------------
-void TutorialApplication::createScene(void)
+void SalamancerApplication::createScene(void)
 {
 }
 
 
+int main(int argc, char *argv[])
+{
+    // Create application object
+    SalamancerApplication app;
 
-#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
-#define WIN32_LEAN_AND_MEAN
-#include "windows.h"
-#endif
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
-    INT WINAPI WinMain( HINSTANCE hInst, HINSTANCE, LPSTR strCmdLine, INT )
-#else
-    int main(int argc, char *argv[])
-#endif
-    {
-        // Create application object
-        TutorialApplication app;
-
-        try {
-            app.go();
-        } catch( Ogre::Exception& e ) {
-#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
-            MessageBox( NULL, e.getFullDescription().c_str(), "An exception has occured!", MB_OK | MB_ICONERROR | MB_TASKMODAL);
-#else
-            std::cerr << "An exception has occured: " <<
-                e.getFullDescription().c_str() << std::endl;
-#endif
-        }
-
-        return 0;
+    try {
+        app.go();
+    } catch( Ogre::Exception& e ) {
+        std::cerr << "An exception has occured: " <<
+            e.getFullDescription().c_str() << std::endl;
     }
-
-#ifdef __cplusplus
+    return 0;
 }
-#endif
+
--- SalamancerApplication.h
+++ SalamancerApplication.h
@@ -1,29 +1,13 @@
-/*
------------------------------------------------------------------------------
-Filename:    TutorialApplication.h
------------------------------------------------------------------------------
-
-This source file is part of the
-   ___                 __    __ _ _    _ 
-  /___\__ _ _ __ ___  / / /\ \ (_) | _(_)
- //  // _` | '__/ _ \ \ \/  \/ / | |/ / |
-/ \_// (_| | | |  __/  \  /\  /| |   <| |
-\___/ \__, |_|  \___|   \/  \/ |_|_|\_\_|
-      |___/                              
-      Tutorial Framework
-      http://www.ogre3d.org/tikiwiki/
------------------------------------------------------------------------------
-*/
-#ifndef __TutorialApplication_h_
-#define __TutorialApplication_h_
+#ifndef __SalamancerApplication_h_
+#define __SalamancerApplication_h_
 
 #include "BaseApplication.h"
 
-class TutorialApplication : public BaseApplication
+class SalamancerApplication : public BaseApplication
 {
 public:
-    TutorialApplication(void);
-    virtual ~TutorialApplication(void);
+    SalamancerApplication(void);
+    virtual ~SalamancerApplication(void);
 
 protected:
     virtual void createScene(void);
