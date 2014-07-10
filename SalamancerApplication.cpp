d051e7617ee1ba0d25cb1dcc87d56230b1009755
Nathan Hartman <nathanial.hartman@gmail.com>
2014-07-10 02:01:24 -0700

    switched to c++11, added ogrehead

--- SalamancerApplication.cpp
+++ SalamancerApplication.cpp
@@ -13,6 +13,13 @@ SalamancerApplication::~SalamancerApplication(void)
 //-------------------------------------------------------------------------------------
 void SalamancerApplication::createScene(void)
 {
+    mSceneMgr->setAmbientLight(Ogre::ColourValue(0.5f, 0.5f, 0.5f));
+    
+    auto ogreHead = mSceneMgr->createEntity("Head", "ogrehead.mesh");
+    auto headNode = mSceneMgr->getRootSceneNode()->createChildSceneNode("HeadNode");
+    headNode->attachObject(ogreHead);
+    auto light = mSceneMgr->createLight("MainLight");
+    light->setPosition(20.0f, 80.0f, 50.0f);
 }
 
 
