2ad2517a96fa602b40df6688d3b3416b1d21bfee
Nathan Hartman <nathanial.hartman@gmail.com>
2014-07-11 00:45:50 -0700

    got some terrain

--- src/SalamancerApplication.cpp
+++ src/SalamancerApplication.cpp
@@ -2,6 +2,7 @@
 #include "framework/terrain/PerlinTerrainGenerator.h"
 #include "framework/Position.h"
 #include "framework/Mesher.h"
+#include "framework/World.h"
 #include <functional>
 
 using namespace Ogre;
@@ -28,7 +29,7 @@ void SalamancerApplication::createScene(void)
     Entity *thisEntity = mSceneMgr->createEntity("cc", "ColourCube");
     thisEntity->setMaterialName("ColorTest");
     SceneNode* thisSceneNode = mSceneMgr->getRootSceneNode()->createChildSceneNode();
-    thisSceneNode->setPosition(-100,0,0);
+    thisSceneNode->setPosition(0,0,-100);
     thisSceneNode->attachObject(thisEntity);
     
     mSceneMgr->setAmbientLight(Ogre::ColourValue(0.5f, 0.5f, 0.5f));
@@ -39,6 +40,13 @@ void SalamancerApplication::createScene(void)
 
 void createColourCube(void)
 {
+    
+    PerlinTerrainGenerator gen;
+    VolumePtr volume = gen.generate(Position(1,World::YCHUNKS -1,1));
+    auto verticesAndFaces = Mesher::mesh(volume);
+    Mesher::Vertices voxVertices = std::get<0>(verticesAndFaces);
+    Mesher::Faces voxFaces = std::get<1>(verticesAndFaces);
+    
     /// Create the mesh via the MeshManager
     Ogre::MeshPtr msh = MeshManager::getSingleton().createManual("ColourCube", "General");
  
@@ -48,26 +56,41 @@ void createColourCube(void)
     const float sqrt13 = 0.577350269f; /* sqrt(1/3) */
  
     /// Define the vertices (8 vertices, each consisting of 2 groups of 3 floats
-    const size_t nVertices = 8;
-    const size_t vbufCount = 3*2*nVertices;
-    float vertices[vbufCount] = {
-            -100.0,100.0,-100.0,        //0 position
-            -sqrt13,sqrt13,-sqrt13,     //0 normal
-            100.0,100.0,-100.0,         //1 position
-            sqrt13,sqrt13,-sqrt13,      //1 normal
-            100.0,-100.0,-100.0,        //2 position
-            sqrt13,-sqrt13,-sqrt13,     //2 normal
-            -100.0,-100.0,-100.0,       //3 position
-            -sqrt13,-sqrt13,-sqrt13,    //3 normal
-            -100.0,100.0,100.0,         //4 position
-            -sqrt13,sqrt13,sqrt13,      //4 normal
-            100.0,100.0,100.0,          //5 position
-            sqrt13,sqrt13,sqrt13,       //5 normal
-            100.0,-100.0,100.0,         //6 position
-            sqrt13,-sqrt13,sqrt13,      //6 normal
-            -100.0,-100.0,100.0,        //7 position
-            -sqrt13,-sqrt13,sqrt13,     //7 normal
-    };
+    size_t nVertices = voxVertices.size();
+    size_t vbufCount = 3*2*nVertices;
+    float vertices[vbufCount]; 
+    for(int i = 0; i < nVertices; i++){
+        int cursor = i*3*2;
+        Mesher::Vertex vertex = voxVertices.at(i);
+        
+        //position
+        vertices[cursor++] = vertex[0];
+        vertices[cursor++] = vertex[1];
+        vertices[cursor++] = vertex[2];
+        
+        //normal
+        vertices[cursor++] = sqrt13 * (vertex[0] < 0 ? -1 : 1);
+        vertices[cursor++] = sqrt13 * (vertex[1] < 0 ? -1 : 1);
+        vertices[cursor++] = sqrt13 * (vertex[2] < 0 ? -1 : 1);
+    }
+//    {
+//            -100.0,100.0,-100.0,        //0 position
+//            -sqrt13,sqrt13,-sqrt13,     //0 normal
+//            100.0,100.0,-100.0,         //1 position
+//            sqrt13,sqrt13,-sqrt13,      //1 normal
+//            100.0,-100.0,-100.0,        //2 position
+//            sqrt13,-sqrt13,-sqrt13,     //2 normal
+//            -100.0,-100.0,-100.0,       //3 position
+//            -sqrt13,-sqrt13,-sqrt13,    //3 normal
+//            -100.0,100.0,100.0,         //4 position
+//            -sqrt13,sqrt13,sqrt13,      //4 normal
+//            100.0,100.0,100.0,          //5 position
+//            sqrt13,sqrt13,sqrt13,       //5 normal
+//            100.0,-100.0,100.0,         //6 position
+//            sqrt13,-sqrt13,sqrt13,      //6 normal
+//            -100.0,-100.0,100.0,        //7 position
+//            -sqrt13,-sqrt13,sqrt13,     //7 normal
+//    };
  
     RenderSystem* rs = Root::getSingleton().getRenderSystem();
     RGBA colours[nVertices];
@@ -84,21 +107,30 @@ void createColourCube(void)
  
     /// Define 12 triangles (two triangles per cube face)
     /// The values in this table refer to vertices in the above table
-    const size_t ibufCount = 36;
-    unsigned short faces[ibufCount] = {
-            0,2,3,
-            0,1,2,
-            1,6,2,
-            1,5,6,
-            4,6,5,
-            4,7,6,
-            0,7,4,
-            0,3,7,
-            0,5,1,
-            0,4,5,
-            2,7,3,
-            2,6,7
-    };
+    size_t ibufCount = voxFaces.size() * 3;
+    unsigned short faces[ibufCount];
+    
+    for(int i = 0; i < voxFaces.size(); i++){
+        int cursor = i * 3;
+        Mesher::Face face = voxFaces[i];
+        faces[cursor++] = face[0];
+        faces[cursor++] = face[1];
+        faces[cursor++] = face[2];
+    }
+//    = {
+//            0,2,3,
+//            0,1,2,
+//            1,6,2,
+//            1,5,6,
+//            4,6,5,
+//            4,7,6,
+//            0,7,4,
+//            0,3,7,
+//            0,5,1,
+//            0,4,5,
+//            2,7,3,
+//            2,6,7
+//    };
  
     /// Create vertex data structure for 8 vertices shared between submeshes
     msh->sharedVertexData = new VertexData();
