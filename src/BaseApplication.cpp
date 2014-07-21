86ce28f17315d38922adcdaacf6c8c5fbc15a016
Nathan Hartman <nathanial.hartman@gmail.com>
2014-07-20 22:52:15 -0700

    disabled camera mouse

--- src/BaseApplication.cpp
+++ src/BaseApplication.cpp
@@ -380,24 +380,24 @@ bool BaseApplication::keyReleased( const OIS::KeyEvent &arg )
 
 bool BaseApplication::mouseMoved( const OIS::MouseEvent &arg )
 {
-    if (mTrayMgr->injectMouseMove(arg)) return true;
-    mCameraMan->injectMouseMove(arg);
+//    if (mTrayMgr->injectMouseMove(arg)) return true;
+//    mCameraMan->injectMouseMove(arg);
     this->onMouseMoved(arg);
     return true;
 }
 
 bool BaseApplication::mousePressed( const OIS::MouseEvent &arg, OIS::MouseButtonID id )
 {
-    if (mTrayMgr->injectMouseDown(arg, id)) return true;
-    mCameraMan->injectMouseDown(arg, id);
+//    if (mTrayMgr->injectMouseDown(arg, id)) return true;
+//    mCameraMan->injectMouseDown(arg, id);
     this->onMousePressed(arg, id);
     return true;
 }
 
 bool BaseApplication::mouseReleased( const OIS::MouseEvent &arg, OIS::MouseButtonID id )
 {
-    if (mTrayMgr->injectMouseUp(arg, id)) return true;
-    mCameraMan->injectMouseUp(arg, id);
+//    if (mTrayMgr->injectMouseUp(arg, id)) return true;
+//    mCameraMan->injectMouseUp(arg, id);
     this->onMouseReleased(arg, id);
     return true;
 }
